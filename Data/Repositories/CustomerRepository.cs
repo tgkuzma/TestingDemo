2266925b24222872765ba75ca6944c89d58b4a57
Trenton <trenton.kuzma@tru-cs.com>
2016-02-12 18:09:45 -0700

    Reentering files that didn't make it to GitHub

---
+++ Data/Repositories/CustomerRepository.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Linq;
+using Models;
+using Models.Interfaces;
+
+namespace Data.Repositories
+{
+    public class CustomerRepository : RepositoryBase<Customer>, ICustomerRepository
+    {
+        private readonly TestingContext _context;
+
+        public CustomerRepository(TestingContext context) : base(context)
+        {
+            _context = context;
+        }
+
+        public Customer GetByDateOfBirth(DateTime dateOfBirth)
+        {
+            return _context.Customers.FirstOrDefault(c => c.DateOfBirth == dateOfBirth);
+        }
+    }
+}
---
+++ Data/TestingContext.cs
@@ -0,0 +1,15 @@
+﻿using System.Data.Entity;
+using Models;
+
+namespace Data
+{
+    public class TestingContext : DbContext
+    {
+        public TestingContext() : base("Testing.DbConnection")
+        {
+            Database.SetInitializer(new DataContextInitializer());
+        }
+
+        public DbSet<Customer> Customers { get; set; }
+    }
+}
