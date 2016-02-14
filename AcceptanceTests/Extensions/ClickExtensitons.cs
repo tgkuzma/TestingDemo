d8313f360599492e32e950e86c5431ba6c6d3183
Trenton <trenton.kuzma@tru-cs.com>
2016-02-14 10:03:41 -0700

    Wrote FAAT for add

---
+++ AcceptanceTests/Extensions/ClickExtensitons.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Threading;
+using OpenQA.Selenium;
+
+namespace AcceptanceTests.Extensions
+{
+    public static class ClickExtensitons
+    {
+        public static void ClickAndWait(this IWebElement element, double timeoutInSeconds)
+        {
+            if (!(timeoutInSeconds > 0)) return;
+            var timeout = timeoutInSeconds * 1000;
+            element.Click();
+            Thread.Sleep(Convert.ToInt32(timeout));
+        }
+    }
+}
