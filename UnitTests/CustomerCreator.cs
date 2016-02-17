c5407903a636c58125fadbc80f7db82d54e1ef32
Trenton <trenton.kuzma@tru-cs.com>
2016-02-16 18:00:46 -0700

    GitHub SUCKS!!! Added missing file back in

---
+++ UnitTests/CustomerCreator.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using Models;
+
+namespace UnitTests
+{
+    public class CustomerCreator
+    {
+        public static Customer CreateSingleCustomer(int age)
+        {
+            return new Customer
+            {
+                DateOfBirth = DateTime.Now.AddYears(-age),
+                FirstName = "Twenty",
+                LastName = "YearsOld",
+                Id = 1
+            };
+        }
+    }
+}
